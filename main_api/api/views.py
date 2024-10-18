0c93642be6718a49c9dfb031c523da1a84a577ae
elrrowwe <248607@edu.p.lodz.pl>
2024-10-19 01:57:51 +0200

    fixed scikit-learn version conflicts

--- main_api/api/views.py
+++ main_api/api/views.py
@@ -11,9 +11,13 @@ from main_api.main_api.ml_model.random_forest_classifier import RandomForestClas
 class LoansPredictionView(APIView):
     permission_classes = [MockedTokenPermission]
 
-    def get(self,
-            request):
-
+    @swagger_auto_schema(
+        operation_summary="Predict the loan amount",
+        operation_description="Predict the loan amount for a given customer (our only customer for now). "
+                              "Pass Authorization header with a Bearer token.",
+        responses={200: "The predicted loan amount"}
+    )
+    def get(self, request):
         clf = RandomForestClassifier()
 
         prediction, data = clf.predict()
@@ -25,24 +29,8 @@ class LoansPredictionView(APIView):
         response = chatbot.get_response(prediction=prediction,
                                         data=data,
                                         tree_rules=tree_rules)
-
         return Response({
             "message": response,
-        })
-
-
-class LoansPredictionView(APIView):
-    permission_classes = [MockedTokenPermission]
-
-    @swagger_auto_schema(
-        operation_summary="Predict the loan amount",
-        operation_description="Predict the loan amount for a given customer (our only customer for now). "
-                              "Pass Authorization header with a Bearer token.",
-        responses={200: "The predicted loan amount"}
-    )
-    def get(self, request):
-        return Response({
-            "message": "Hello, world!",
         })
 
 
