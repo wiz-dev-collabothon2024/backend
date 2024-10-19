aed15840023b1dcf841a502fb5a64d763ca38b9e
a-vishniavetski <a.vishniavetski@gmail.com>
2024-10-19 15:00:07 +0200

    feat: change main balance data format

--- main_api/api/views.py
+++ main_api/api/views.py
@@ -67,13 +67,13 @@ class MainBalanceManyAccounts(APIView):
     )
     def get(self, request):
         response_data = [
-            {self.ACCOUNT_NAME + "1": "First account", self.BALANCE_NAME + "1": 5_836_482},
-            {self.ACCOUNT_NAME + "2": "Second account", self.BALANCE_NAME + "2": 15_104_856},
-            {self.ACCOUNT_NAME + "3": "Third account", self.BALANCE_NAME + "3": 55_536_433},
-            {self.ACCOUNT_NAME + "4": "Fourth account", self.BALANCE_NAME + "4": 1_536_433},
-            {self.ACCOUNT_NAME + "5": "Fifth account", self.BALANCE_NAME + "5": 32_957_611},
-            {self.ACCOUNT_NAME + "6": "Sixth account", self.BALANCE_NAME + "6": 13_954_414},
-            {self.ACCOUNT_NAME + "7": "Seventh account", self.BALANCE_NAME + "7": 55_957_001},
+            {self.ACCOUNT_NAME: "First account", self.BALANCE_NAME: 5_836_482},
+            {self.ACCOUNT_NAME: "Second account", self.BALANCE_NAME: 15_104_856},
+            {self.ACCOUNT_NAME: "Third account", self.BALANCE_NAME: 55_536_433},
+            {self.ACCOUNT_NAME: "Fourth account", self.BALANCE_NAME: 1_536_433},
+            {self.ACCOUNT_NAME: "Fifth account", self.BALANCE_NAME: 32_957_611},
+            {self.ACCOUNT_NAME: "Sixth account", self.BALANCE_NAME: 13_954_414},
+            {self.ACCOUNT_NAME: "Seventh account", self.BALANCE_NAME: 55_957_001},
         ]
         return Response(response_data)
 
